01754df2fa4bd0061701cae234fca98239fca345
Alpha Geek <alpha0geek@gmail.com>
2016-08-01 20:14:40 +0500

    .

--- index.php
+++ index.php
@@ -1,42 +1,17 @@
 <?php
 
-require dirname( __FILE__ ) . '/vendor/autoload.php';
-
-use Aws\Sns\SnsClient;
-use Aws\Sns\Message;
-use Aws\Sns\MessageValidator;
-
-
     $body = '';
     while (FALSE !== ($line = fgets(STDIN))) {
 		$body.= $line;
     }
-	
- 	//echo "body" . $body;
-	
- 	//$body = file_get_contents('mess.txt');
-    $data = json_decode($body,true);
-	$data = $data['Records'][0]['Sns'];
-	
-	//print_r( $data); 
-	//exit;
-	
-try {
-	
-	$message = new Message($data);
-	
-	/*
-	$validator = new MessageValidator();
-	if ($validator->isValid($message)) {
-		echo "Sns Message Received: " . $message->get('Message'); 
-	}
-	*/
-	
-	echo "Sns Message Received: " . $message->get('Message');
-	
-} catch (Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
-}
 
+ 	//echo "body" . $body;
+    $data = json_decode($body,true);
+	$message = $data['Records'][0]['Sns']["Message"];
+	
+	echo "Sns Message Received: " . $message;
+	//print_r( $data); 
+	exit;
+	
 
 ?>
