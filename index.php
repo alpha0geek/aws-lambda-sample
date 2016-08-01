dae37448e23e1fe81ab6a08abe8fddcf9ce32772
Alpha Geek <alpha0geek@gmail.com>
2016-08-01 21:17:31 +0500

    .

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+define('DB_SERVER', 'lamdadb.cwzhqacjpnfs.ap-southeast-2.rds.amazonaws.com:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'snsmess321');
+define('DB_DATABASE', 'lambdaDB');
+
 
     $body = '';
     while (FALSE !== ($line = fgets(STDIN))) {
@@ -16,4 +21,23 @@
 	//flush();
 	//print_r( $data);
 
+	
+
+	$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+			
+	$sql = "INSERT INTO messages (id, message) VALUES (NULL, '" . $message . "')";
+
+	if ($conn->query($sql) === TRUE) {
+	    echo "New SNS message recorded in RDS DB.";
+	} else {
+	    echo "Error: " . $sql . " -> " . $conn->error;
+	}
+
+	$conn->close();
+
+	
 ?>
