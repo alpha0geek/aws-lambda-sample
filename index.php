ebfd86f8da7a5f106aa2e261a32de55cb3cce5bc
Alpha Geek <alpha0geek@gmail.com>
2016-08-01 19:43:15 +0500

    .

--- index.php
+++ index.php
@@ -13,10 +13,12 @@ use Aws\Sns\MessageValidator;
     }
 
  
+ 	echo $body;
+	exit;
  	//$body = file_get_contents('mess.txt');
     $data = json_decode($body,true);
 	
-	//print_r( $data);
+	print_r( $data);
 	//exit;
 	
 try {
